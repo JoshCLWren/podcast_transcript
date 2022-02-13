12bf1ab1c7824a39bd226b0f5bc73b7ef4eff4aa
Josh Wren <josh.wren@life.church>
2022-02-13 12:51:50 -0600

    extracted file downloading from audio_conversion.py::wav_converter to its own method

--- audio_conversion.py
+++ audio_conversion.py
@@ -11,18 +11,16 @@ def wav_converter(url, title, format="mp3"):
     """
     Converts an audio file to a mono wav file and normalizes it.
     """
-    start_time = time.time()
-    folder_name = f"{title}"
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
-    output_file = f"/tmp/{title}.mp3"
-    urllib.request.urlretrieve(url, output_file)
-    download_time = time.time() - start_time
-    print(f"Download time: {download_time}")
-    if format == "mp3":
-        song = pydub.AudioSegment.from_mp3(output_file)
-    else:
-        song = pydub.AudioSegment.from_file(f"{output_file}", format=format)
+    start_time, folder_name, output_file = download_resource(url, title, format)
+
+    try:
+        if format == "mp3":
+            song = pydub.AudioSegment.from_mp3(output_file)
+        else:
+            song = pydub.AudioSegment.from_file(f"{output_file}", format=format)
+    except:
+        raise Exception("Could not convert to wav")
+
     wav_file = f"{title}.wav"
     song = song.set_channels(1)
     song = song.set_frame_rate(16000)
@@ -31,17 +29,33 @@ def wav_converter(url, title, format="mp3"):
     end_time = time.time()
     total_time = end_time - start_time
     print(f"Time to convert mp3 to wav: {total_time}")
-    shutil.rmtree(folder_name)
+    shutil.rmtree("tmp")
     return song.export(wav_file, format="wav")
+
+
+def download_resource(url, title, format):
+    """Downloads a resource from a url and returns the path to the file."""
+    start_time = time.time()
+
+    folder_name = f"tmp/{title}"
+    if not os.path.isdir("tmp"):
+        os.mkdir("tmp")
+    if not os.path.isdir(folder_name):
+        os.mkdir(f"tmp/{title}")
+    output_file = f"{folder_name}/{title}.{format}"
+    urllib.request.urlretrieve(url, output_file)
+    download_time = time.time() - start_time
+    print(f"Download time: {download_time}")
+    return start_time, folder_name, output_file
 
 
 def video_to_audio(url):
     """
-    Converts a youtube file to a mono wav file.
+    Prepares a youtube url to be converted to a wav.
     """
 
     stream_url = pytube.YouTube(url).streams.filter(only_audio=True).first().url
     title = f"{pytube.YouTube(url).title}"
     print(f"Downloading {title}")
     regexed_title = re.sub(r"[^a-zA-Z0-9]", "", title)
-    return wav_converter(stream_url, regexed_title, format="mp4")
+    return wav_converter(url=stream_url, title=regexed_title, format="mp4")
